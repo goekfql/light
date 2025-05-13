ef0482fa0afd67795efa13ba339ac2f7af07f7af
Jacob <62026743+goekfql@users.noreply.github.com>
2025-05-13 16:58:20 +0900

    Update main.js

--- empty-map/main.js
+++ empty-map/main.js
@@ -1,9 +1,11 @@
-// OpenLayers 맵 초기화
 const map = new ol.Map({
     target: 'map',
     layers: [
         new ol.layer.Tile({
-            source: new ol.source.OSM()
+            source: new ol.source.XYZ({
+                url: 'https://cartodb-basemaps-{a-d}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
+                attributions: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            })
         })
     ],
     view: new ol.View({
